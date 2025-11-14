38a490006ab7174827477e0bcba8d6cd58fb4556
artus9033 <artus9033@gmail.com>
2025-11-14 14:27:58 +0100

    chore: bootstrap empty activity Android app

---
+++ BrownfieldTestApp-android/app/src/main/java/com/workshops/brownfieldtestapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.workshops.brownfieldtestapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.workshops.brownfieldtestapp.ui.theme.BrownfieldTestAppandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BrownfieldTestAppandroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BrownfieldTestAppandroidTheme {
+        Greeting("Android")
+    }
+}
