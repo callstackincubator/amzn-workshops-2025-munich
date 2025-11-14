9cd2d9f1f35af9f9a35b61a36ba9d1012e5398ba
artus9033 <artus9033@gmail.com>
2025-11-14 15:13:56 +0100

    chore: bootstrap android Rock RN app

---
+++ RockRNApp/ios/RockRNAppReact/RockRNAppReact.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for RockRNAppReact.
+FOUNDATION_EXPORT double RockRNAppReactVersionNumber;
+
+//! Project version string for RockRNAppReact.
+FOUNDATION_EXPORT const unsigned char RockRNAppReactVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RockRNAppReact/PublicHeader.h>
